96a19069fc81e89baf65c51148597c036523090d
Thom Valadez <thomas.valadez@getweave.com>
2019-11-22 16:17:00 -0700

    added affiliate link

--- src/components/nav-controls.component.tsx
+++ src/components/nav-controls.component.tsx
@@ -10,25 +10,29 @@ export const NavControls = () => {
     const history = useHistory();
     return (
         <>
-            {userState.user.uid === '' && !loading &&
-                <div className="login-container">
-                    <button className="login" onClick={login}> Sign In</button>
+            {loading
+                ? <Loading />
+                :
+                <div className="amazon-banner">
+                    <div className={userState.user.uid === '' ? "nav-button-wrapper-logged-out" : "nav-button-wrapper-logged-in"}>
+                        {userState.user.uid === '' && !loading &&
+                            <div className="login-container">
+                                <button className="login" onClick={login}> Sign In</button>
+                            </div>
+                        }
+                        {userState.user.uid !== '' &&
+                            <>
+                                {history.location.pathname === '/create-game'
+                                    ? <button onClick={() => { history.push('/') }}>active games</button>
+                                    : <button onClick={() => { history.push('/create-game') }}>organize games</button>
+                                }
+                            </>
+                        }
+                        <iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=26&l=ur1&category=amazonhomepage&f=ifr&linkID=3bbe1b0cd2947cd880f38191e5cc9720&t=thmcodes-20&tracking_id=thmcodes-20" width="468" height="60" scrolling="no" marginWidth={0} style={{ border: "none;" }} frameBorder="0"></iframe>
+                        {userState.user.uid !== '' && <button onClick={logout}>Sign out</button>}
+                    </div>
                 </div>
             }
-            <div className="nav-button-wrapper">
-                {userState.user.uid !== '' &&
-                    <div>
-                        {history.location.pathname === '/create-game'
-                            ? <button onClick={() => { history.push('/') }}>active games</button>
-                            : <button onClick={() => { history.push('/create-game') }}>organize games</button>
-                        }
-                        <button className="logout" onClick={logout}>Sign out</button>
-                    </div>
-
-                }
-            </div>
-            {/* <h3>Secret Santanator Game 3000-v0.2</h3> */}
-            {loading && <Loading />}
         </>
     )
 }
